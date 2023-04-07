5f3609572c2959a5c3f2c7a846660e5480e4889b
EliseTomlin <edj46760@gmail.com>
2023-04-06 23:19:04 -0500

    added new cw challenge 4/6

---
+++ 8kyu/reduce-grow.js
@@ -0,0 +1,23 @@
+// Given a non-empty array of integers, return the result of multiplying the values together in order. Example:
+
+// [1, 2, 3, 4] => 1 * 2 * 3 * 4 = 24
+
+//my solution
+
+function grow(x){
+    let result = 1
+    for(let i = 0; i<x.length; i++){
+        result *= x[i]
+    }return result
+}
+
+//other ans
+function grow(x){
+    result =1
+    x.map(x=>{
+        result *= x
+    })
+    return result
+}
+//=========================
+const grow=x=> x.reduce((a,b) => a*b);
