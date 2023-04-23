06201a82fe62ca9debd5c36519e5d9a156f90294
EliseTomlin <edj46760@gmail.com>
2023-04-22 22:49:14 -0500

    adding new cw challenge

---
+++ 8kyu/third-angle-of-triangle.js
@@ -0,0 +1,20 @@
+// You are given two interior angles (in degrees) of a triangle.
+
+// Write a function to return the 3rd.
+
+// Note: only positive integers will be tested.
+
+
+
+//mysolution
+
+function otherAngle(a, b) {
+    let angle = 180 - (a+b) 
+    return angle
+    }
+
+
+
+    //other anwsers
+
+    const otherAngle = (a, b) => 180 - a - b
