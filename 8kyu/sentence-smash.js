9ec544157de56e1290646d061df0a30cff07ec49
EliseTomlin <edj46760@gmail.com>
2023-05-01 02:10:34 -0500

    added a new cw

---
+++ 8kyu/sentence-smash.js
@@ -0,0 +1,27 @@
+// Sentence Smash
+// Write a function that takes an array of words and smashes them together into a sentence and returns the sentence. You can ignore any need to sanitize words or add punctuation, but you should add spaces between each word. Be careful, there shouldn't be a space at the beginning or the end of the sentence!
+
+
+//mysolutoin
+function smash (words) {
+    return words.join(' ')
+ };
+
+
+ //otheranswers
+
+ let smash = words => words.join(" ");
+//=====================
+
+
+function smash (words) {
+    "use strict";
+    var smashed = '';
+    for(var i = 0; i<words.length; i++) {
+      smashed += words[i];
+      if(i!=words.length-1) {
+        smashed += ' ';
+      }
+    }
+    return smashed;
+};
