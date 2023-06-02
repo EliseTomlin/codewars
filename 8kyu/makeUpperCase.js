04913eb7ca708e58cf47b3083d8f209004d4563b
EliseTomlin <edj46760@gmail.com>
2023-06-01 23:00:16 -0500

    added new cw challenge

---
+++ 8kyu/makeUpperCase.js
@@ -0,0 +1,11 @@
+//Write a function which converts the input string to uppercse
+
+//mysolution
+
+makeUpperCase = str => str.toUpperCase()
+
+//other answers
+
+function makeUpperCase (str){
+    return str.toUpperCase()
+}
