773eb6f66f4f4a4d8ebd5ba0cd83498a94bb5443
EliseTomlin <edj46760@gmail.com>
2023-06-19 23:56:07 -0500

    added new cw challenge

---
+++ 8kyu/square-argument.js
@@ -0,0 +1,11 @@
+// Now you have to write a function that takes an argument and returns the square of it.
+
+//my solution
+
+function square(num){
+    return Math.pow(num,2)
+}
+
+//other answ
+
+const square=(n)=>n*n
